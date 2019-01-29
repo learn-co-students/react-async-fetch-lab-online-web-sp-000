e3d441c45abfe1bd74778c36e92a984bf35d0e4b
danmj <daniel.jung@stern.nyu.edu>
2019-01-29 14:08:04 -0500

    da

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // create your App component here
 import React, { Component } from 'react'
 
-class App extends Component {
+export default class App extends Component {
 
   state = {
       peopleInSpace: []
@@ -23,5 +23,3 @@ class App extends Component {
   }
 
 }
-
-export default App
