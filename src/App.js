1cfe7a0107559f54f02480166cbc1dd334994778
congo-quinine <congo.quinine@gmail.com>
2019-02-07 14:41:58 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import React, {Component} from 'react'
+
 // create your App component here
+class App extends Component {
+
+
+  componentDidMount(){
+    const URL = ' http://api.open-notify.org/astros.json'
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+  }
+
+  render(){
+    <div>
+    </div>
+  }
+
+}
+
+export default App;
