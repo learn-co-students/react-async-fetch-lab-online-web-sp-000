5f98a5f1136eb19c62da9ae8d1d0ea3c1a347942
fut0k0 <cccabell@icloud.com>
2019-10-04 11:49:46 -0700

    Passing lab

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
-// create your App component here
+import React from "react";
+
+class App extends React.Component {
+
+	componentDidMount() {
+		fetch("http://api.open-notify.org/astros.json");
+	}
+
+	render() {
+		return null;
+	}
+
+}
+
+export default App;
