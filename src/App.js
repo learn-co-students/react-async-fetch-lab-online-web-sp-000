9c37b5a02df71d22d7b98a3efe2b08d0a54fc8c8
fut0k0 <cccabell@icloud.com>
2019-10-07 10:41:41 -0700

    Passing lab

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React from "react";
 
 class App extends React.Component {
 
+	state = {
+		peopleInSpace: []
+	}
+
 	componentDidMount() {
-		fetch("http://api.open-notify.org/astros.json");
+		fetch("http://api.open-notify.org/astros.json")
+			.then(response => response.json())
+			.then(data => {
+				this.setState({
+					peopleInSpace: data.people
+				});
+			});
 	}
 
 	render() {
-		return null;
+		return (
+			<div>
+				{this.state.peopleInSpace.map((person, index) => {
+					return (
+						<div key={index}>
+							{person.name}
+							,{" "} 
+							{person.craft}
+						</div>
+					);
+				})}
+			</div>
+		);
 	}
 
 }
