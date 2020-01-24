99a42c5b3e0cebb4d4609c0b703fac4d61bd20e5
Tania Manco <belalov.tania16@gmail.com>
2020-01-24 20:54:36 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
-// create your App component here
+import React from 'react';
+
+class App extends React.Component {
+  
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => resp.json())
+    .then(peopleInSpace => console.log(peopleInSpace))
+  }
+  
+  
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
+
+export default App;
