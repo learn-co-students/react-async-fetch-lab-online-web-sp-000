1cc23b6053ef965f4b7242931663e3f8f3b63081
Educhin <c.s.arinze@gmail.com>
2020-05-22 21:32:51 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { Component } from "react"
+
+export default class App extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            spacePeeps: []
+        }
+    }
+
+    setPeeps = (peeps) => {
+        this.setState({
+            spacePeeps: peeps    
+        })
+    }
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(response => response.json())
+        .then(data => {
+            this.setPeeps(data.people)
+        })
+    }
+
+    render() {
+        return (
+            <div>{this.state.spacePeeps.map(person => person.name)}</div>
+        )
+    }
+}
