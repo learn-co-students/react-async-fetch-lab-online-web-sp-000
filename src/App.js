eb7a908e973e8d3cce0d4b9fe1a162d5bd491f32
Ben Tagtow <bentagtow@gmail.com>
2020-05-25 11:52:02 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -4,28 +4,22 @@ import React, { Component } from 'react'
 class App extends Component {
     state = {
         message: "",
-        people: []
     }
     componentDidMount(){
         fetch('http://api.open-notify.org/astros.json')
             .then(response => response.json())
             .then(data => {
                 this.setState({
-                    message: data.message,
-                    people: data.message.name
-                })
+                    message: data.message                })
             })
     }
     render(){
         return (
-
             <div>
                 <h1>
                     {this.state.message}
                 </h1>
-                <h2>
-                    {this.state.people}
-                </h2>
+
             </div>
         )
     }
