537d949707602d07aade25b603461f485714dd31
github username <github email address>
2020-06-17 10:26:36 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      people: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ people: data.people });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {" "}
+        {this.state.people.map(person => (
+          <p>{person.name}</p>
+        ))}{" "}
+      </div>
+    );
+  }
+}
+
+export default App;
