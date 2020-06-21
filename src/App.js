40b18264d439f4831ac03d1dac1a6853f58f317c
theVTOQ <vonfunkhouserprod@gmail.com>
2020-06-21 14:39:20 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export default class App extends React.Component {
+
+    // constructor(){
+    //     this.state = {
+    //         astros: []
+    //     }
+    // }
+
+    state = {
+        astros: []
+    }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                astros: json.people
+            })
+        })
+        .catch(error => alert(error.message));
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.astros.map(astro => astro.name)}
+            </div>
+        )
+    }
+}
