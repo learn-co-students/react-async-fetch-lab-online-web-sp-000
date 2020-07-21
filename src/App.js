dfad61b69e8f274e53d0f2a2d06569b42c3b4f69
Dan-zai <danielcowpertwait@gmail.com>
2020-07-21 11:03:58 +1000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import Reactimport, {Component} from 'react';
+
+export default class App extends Component {
+    state ={
+        astronauts: []
+    }
+
+
+    render() {
+        return this.state.astronauts.map(astronauts => {
+            <li>{astronaut.name}</li>
+        })
+    }
+
+    componentDidMount() {
+        fetch(`http://api.open-notify.org/astros.json`).then(response =>
+        response.json())
+        .then(res => this.state({astronaut: res.people}))
+    }
+
+}
+
