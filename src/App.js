b214df6928568a12e325b60c022c309c7d061756
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-12 23:25:14 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,43 @@
-// create your App component here
+import React, { Componenent } from 'react'
+
+class App extends Componenent{
+
+  constructor(){
+    super()
+    this.state = {
+      people: []
+    }
+  }
+}
+
+componentDidMount(){
+        const url = `http://api.open-notify.org/astros.json`
+
+        fetch(url)
+        .then(res => {
+            if(!res.ok) throw res.statusText
+            return res.json()
+        })
+        .then(data => {
+            const {people, number, message } = data
+            this.setState({ people })
+        })
+        .catch(e => {
+            alert(e)
+        })
+
+    }
+
+
+    constructPeople = () => this.state.people.map(p => <Person name={p.name} craft={p.craft}/>)
+
+    render(){
+
+        return(
+            <div className="PeopleContainer">
+                {this.constructPeople()}
+            </div>
+
+        )
+    }
+}
