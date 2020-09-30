9e491f9f6b473048cb1635c116a7ef6c906fed32
Cole Rau <colevrau@gmail.com>
2020-09-29 19:29:02 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ export default class App extends React.Component {
 
     }
 
+    state = {
+        peopleInSpace: []
+    }
+
     componentDidMount() {
         fetch("http://api.open-notify.org/astros.json")
         .then(resp => resp.json())
         .then(data => {
-            return data.number;
+            this.setState({
+                peopleInSpace: data.number
+            })
         })
     }
 
     render() {
         return(
-            null
+            <h1>
+                There are {this.state.peopleInSpace} people in space. 
+            </h1>
         )
     }
 }
