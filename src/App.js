18016cc33f7794cecd8551a3234767a7f0714636
Will Carter <will.carter@fergusllc.com>
2020-11-12 14:32:29 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,35 @@
 // create your App component here
+import React, {Component} from "react"
+
+class App extends Component {
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json").then((response) => {
+            return response.json()
+        }).then((data) => {
+            console.log('data', data)
+        }) 
+    }
+
+    // componentDidMount = () => {
+    //     fetch("http://api.open-notify.org/astros.json").then((response) => {
+    //         return response.json()
+    //     }).then((data) => {
+    //         console.log('data', data)
+    //     })
+    // }
+
+    // componentDidMount = () => {
+    //     fetch("http://api.open-notify.org/astros.json").then(response => response.json()).then(data => {
+    //         console.log('data', data)
+    //     })
+    // }
+
+    render() {
+
+        return <div>App</div>
+
+    }
+}
+
+export default App
