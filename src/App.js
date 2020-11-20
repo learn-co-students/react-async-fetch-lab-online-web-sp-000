87f6a022a1b21271a8d8d0c7ff7348093ed07757
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-11-20 00:46:45 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			peopleInSpace: []
+		}
+	}
+
+
+	componentDidMount() {
+		fetch('http://api.open-notify.org/astros.json')
+		.then(resp => resp.json())
+		.then(data => {
+			this.setState({
+				peopleInSpace: data.people
+			})
+		})
+	}
+
+	renderPerson = (person) => {
+		return <div>{person.name}</div>
+	}
+
+	render () {
+		return (
+			<div>{this.state.peopleInSpace.map(person => this.renderPerson(person))}</div>
+		)
+	}
+}
+
+export default App;
