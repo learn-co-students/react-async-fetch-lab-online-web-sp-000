32aa6782b4dd534dec8b9023023f2e1c725f92bf
David Strauch <dmaster18@gmail.com>
2021-01-18 03:40:04 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+
+import React, { Component } from "react";
+
+
+export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {astros_data: []}
+  }
+  
+  fetchWords() {
+    return fetch('http://api.open-notify.org/astros.json').then(response => response.json());
+  }
+  
+  componentDidMount() {
+    this.setState(
+      {astros_data: this.fetchWords()}
+      )
+  }
+  
+  render() {
+    return(
+      <div> {this.state.astros_data}</div>
+    )
+  }
+  
+}
