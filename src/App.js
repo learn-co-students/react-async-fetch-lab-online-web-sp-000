88b367d470a2bd44eca44a6cbf4ec3ba3ba3fec1
francis mendez <fmendez.dev@gmail.com>
2021-01-20 12:21:01 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+
+import React from "react"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  componentDidMount() {
+      fetch('http://api.open-notify.org/astros.json')
+  }
+
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default App;
