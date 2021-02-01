be98b8089dd9a028b80b4dcfc515e7e2f3fde953
The-Gentleman <zraja4402@gmail.com>
2021-02-01 09:46:45 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import { render } from 'enzyme'
+import React, { Component } from 'react'
+
+class App extends Component {
+        state = {
+            spacePeople: []
+        }
+
+    render(){
+        return (
+            <div>
+                {this.state.spacePeople.map(person => person.name)}
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+            .then(response => response.json())
+            .then(({people}) => this.setState({ spacePeople: people }))
+
+    }
+}
+export default App
