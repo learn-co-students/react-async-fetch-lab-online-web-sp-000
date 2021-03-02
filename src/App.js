cf742fcdfb92e8785522b8b9e7180a6a07a10c71
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-03-02 12:28:00 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import React, {Component} from 'react'
+
+export default class App extends React.Component {
+    
+
+    state = {
+        peopleInSpace: []
+    }
+    
+    componentDidMount(){
+        const spaceURL = 'http://api.open-notify.org/astros.json'
+        fetch(spaceURL)
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                peopleInSpace: data.people
+            })
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <ul>
+                    <lh><b>People Currently Floating in the Ether of Space</b></lh>
+                    {this.state.peopleInSpace.map(person => <li>{person.name}</li>)}
+                </ul>
+            </div>
+        )
+    }
+
+}
