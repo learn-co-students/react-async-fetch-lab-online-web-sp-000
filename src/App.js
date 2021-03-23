c7481f2fb502fb2967ef3640dc0cf3f21009991f
Micah Banschick <micha.ban@gmail.com>
2021-03-23 17:04:43 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            data: null
+        }
+    }
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json() )
+        .then(json => this.setState({data: json}))
+    }
+    render(){
+
+    }
+} 
