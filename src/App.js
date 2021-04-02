aaec2f7f080ec94517820fce020de1858050fcd7
khoinguyenkc <khoinguyenkc@gmail.com>
2021-04-01 23:49:14 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { Component} from 'react';
+
+export default class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      list: [],
+      linda: "hiiii"
+    }
+  }
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json').then( resp => resp.json() ).then( json => {
+      this.setState({
+        list: json,
+        linda: 'zuiganchet'
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hi ca nha yeu cua kem</h1>
+        {this.state.linda}
+      </div>
+    )
+  }
+
+
+}
