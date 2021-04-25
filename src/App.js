5b07a590e78e600cc9f7ba298178535850209ba7
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-25 00:45:58 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, {Component} from 'react'
+
+export default class App extends Component {
+    state = {
+        astronauts: []
+    }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(({people}) => this.setState({ astronauts: people }))
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.astronauts.map((person) => <h1>{person.name}</h1>)}
+            </div>
+        )
+    }
+}
