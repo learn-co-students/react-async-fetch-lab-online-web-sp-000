ca441d5c75475a70075ce6bba6876c5ed972e001
catprice444 <catprice444@gmail.com>
2021-05-04 06:02:50 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+// - Create an `App` class component from scratch
+// - Write a `componentDidMount` method. Inside the method, send a `fetch` request to
+//   http://api.open-notify.org/astros.json, a free API that returns a list of people
+//   currently in space.
+
+import React, {Component} from 'react'
+
+export default class App extends Component {
+
+    state = {
+        spacePeople: []
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.spacePeople.map(person => person.name)}
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        fetch(`http://api.open-notify.org/astros.json`)
+        .then(response => response.json)
+        .then(data => {
+            setState({
+                spacePeople: data.people
+            })
+        })
+    }
+
+}
